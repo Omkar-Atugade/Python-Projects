1d248f6cb26fc553fea40ba62ac243d2730badd7
Omkar Atugade <65584225+Omkar-Atugade@users.noreply.github.com>
2020-07-27 09:14:14 +0530

    Add files via upload

---
+++ project2------introduction of you.py
@@ -0,0 +1,24 @@
+#Taking name as input
+
+name = input("Enter your Name : ")
+
+#Taking age as input
+
+age = int(input("Enter your Age : "))
+
+#Taking city of user as a input
+
+city = input("Enter your city : ")
+
+#Ask user what they enjoy
+
+hobby = input("Enter your hobby : ")
+
+#Creating output
+
+x = "Your name is {} and you are {} year old. You are from {} and your hobby is {}.".format(name,age,city,hobby)
+
+
+#Printing output
+
+print(x)
